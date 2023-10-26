a14784a3e1355bfa91d65ba1154b06426dbe5987
Melina Schild <melina.schild@icloud.com>
2023-10-26 13:52:16 +0200

    data fetch

---
+++ react-wetter-app/src/pages/Home.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import geodata from "../assets/geodata.json";
+
+const Home = () => {
+  const [weatherData, setWeatherData] = useState();
+  const [lat, setLat] = useState();
+  const [lon, setLon] = useState();
+
+  console.log({ geodata });
+
+  const getGeodata = (city) => {
+    const selected_city = geodata.find((item) => item.city === city);
+    selected_city
+      ? (setLat(selected_city.lat), setLon(selected_city.lon))
+      : console.log("Stadt nicht gefunden");
+    console.log({ selected_city });
+    console.log("lat, lon:", lat, lon);
+  };
+
+  useEffect(() => {
+    const fetch_link = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=de&appid=14198a73da3c334daf2c8cc21dfb50db&units=metric`;
+    console.log({ fetch_link });
+
+    fetch(fetch_link)
+      .then((response) => response.json())
+      .then((data) => setWeatherData(data))
+      .catch((error) =>
+        console.error("Fehler beim laden der Wetterdaten:", error)
+      );
+    // useEffect wird jedes mal aktualisiert wenn lat oder lon sich ändern
+  }, [lat, lon]);
+  console.log({ weatherData });
+
+  return (
+    <section>
+      <article>
+        <button onClick={() => getGeodata("Berlin")}>Berlin</button>
+        <button onClick={() => getGeodata("Hamburg")}>Hamburg</button>
+        <button onClick={() => getGeodata("Munich")}>München</button>
+        <button onClick={() => getGeodata("Dusseldorf")}>Düsseldorf</button>
+      </article>
+
+      <div className="beschreibung">
+        <p>{weatherData.weather[0].description}</p>
+      </div>
+      <div className="icon">
+        <img
+          src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+          alt="icon"
+        />
+      </div>
+      <div className="temperatur">
+        Aktuell: {weatherData.main.temp.toFixed()} °C
+      </div>
+      <div className="windgeschwindigkeit">
+        Windgeschwindigkeit: {(weatherData.wind.speed * 3.6).toFixed()} km/h
+      </div>
+    </section>
+  );
+};
+
+export default Home;
